988851c4f1bf4c5838bc6bb92bece2378343503e
Luca Kato <lucakato@umich.edu>
2024-01-30 18:16:45 -0500

    everything before hume call works

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+import os
+import secrets
+from flask import Flask, render_template, flash, request, redirect, url_for, jsonify
+from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
 from hume import HumeBatchClient
 from hume.models.config import FaceConfig
+from pprint import pprint
+
+# Load environment variables from .env
+load_dotenv()
+api_key = os.getenv("API_KEY")
+secret_key = os.getenv("FLASK_SECRET_KEY")
 
 app = Flask(__name__)
+# specify upload folder
+app.config['UPLOAD_FOLDER'] = os.getcwd() + '/image_uploads/'
+app.config['SESSION_TYPE'] = 'filesystem'
+# # secret key for session management
+app.secret_key = secret_key
 
 @app.route('/')
 def index():
@@ -10,14 +25,38 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    print('post route')
-    if 'image' in request.files:
+    if request.method == 'POST':
+        print('in POST if block')
+        # check if the post request has the file part
+        if 'image' not in request.files:
+            flash('No image part')
+            return redirect(url_for('index'))
         image = request.files['image']
-        # Replace the following logic with your API call using the image file
-        # For demonstration, just returning a success message
-        return jsonify({'message': 'Image uploaded successfully'})
+        print('after getting image')
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if image.filename == '':
+            flash('No selected image')
+            return redirect(url_for('index'))
+        if image:
+            print('before secure image')
+            imagename = secure_filename(image.filename)
+            print('before save')
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], imagename))
+            #return redirect(url_for('download_file', name=filename))
+            # call to Hume API
+            print('before hume call')
+            client = HumeBatchClient(api_key)
+            config = FaceConfig()
+            print('before job submit')
+            job = client.submit_job(image, [config])
+            predictions = job.get_predictions()
+            pprint(predictions)
+        
+        print('end')
+        return 'end'
     else:
-        return jsonify({'error': 'No image file received'})
-
+        # If it's a GET request, redirect to the index route
+        return redirect(url_for('index'))
 if __name__ == '__main__':
     app.run(debug=True)
