8794cfd9eb39fd0c58c86d661916e6b748a8a76a
Luca Kato <lucakato@umich.edu>
2024-01-30 18:50:28 -0500

    modified utilities function to return dict

--- utilities.py
+++ utilities.py
@@ -1,7 +1,12 @@
 from typing import Any, Dict, List
-
+from collections import defaultdict
 
 def print_emotions(emotions: List[Dict[str, Any]]) -> None:
     emotion_map = {e["name"]: e["score"] for e in emotions}
+    res = defaultdict(float)
     for emotion in ["Joy", "Sadness", "Anger"]:
-        print(f"- {emotion}: {emotion_map[emotion]:4f}")
+        res[emotion] += emotion_map[emotion]
+        #print(f"- {emotion}: {emotion_map[emotion]:4f}")
+    
+    #print(res)
+    return res
